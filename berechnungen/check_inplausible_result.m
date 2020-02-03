3c99e63c75ab7066c99d4499d449c3676d1c8e15
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-02-03 18:59:28 +0100

    Auswertung des letzten Versuchs

--- berechnungen/check_inplausible_result.m
+++ berechnungen/check_inplausible_result.m
@@ -1,13 +1,13 @@
 clear
 clc
 repodir = fullfile(fileparts(which('structgeomsynth_path_init.m')), 'dimsynth', 'results');
-optname = 'IFToMMDACH_Vgl_Winkel45_KondLim1_20200129_Wdh2';
-robname = 'P6PRRRRR6G8P3A1';
-robnr = 4;
+optname = 'IFToMMDACH_Vgl_Winkel10_20200131_nachts_Wdh8';
+robname = 'P6PRRRRR6V2G8P3A1';
+robnr = 3;
 resdir = fullfile(repodir, optname);
 
 tmp = load(fullfile(resdir, sprintf('Rob%d_%s_Endergebnis.mat', robnr, robname)), ...
-  'RobotOptRes', 'Set', 'Traj');
+  'RobotOptRes', 'Set', 'Traj', 'PSO_Detail_Data');
 % tmp.RobotOptRes.vartypes
 parroblib_addtopath({tmp.RobotOptRes.Structure.Name});
 tmp.RobotOptRes.fitnessfcn(tmp.RobotOptRes.p_val)
@@ -15,3 +15,6 @@ tmp.RobotOptRes.fitnessfcn(tmp.RobotOptRes.p_val)
 % 
 x_test = tmp.RobotOptRes.p_val(:);
 
+tmp.PSO_Detail_Data.comptime
+tmp.PSO_Detail_Data.fval
+tmp.RobotOptRes.exitflag
--- berechnungen/results_repeat_evals.m
+++ berechnungen/results_repeat_evals.m
@@ -6,8 +6,8 @@ clc
 
 dimsynthpath = fileparts(which('structgeomsynth_path_init.m'));
 importdir = fullfile(dimsynthpath, 'dimsynth', 'results');
-filter = 'IFToMMDACH_Vgl_Winkel*_KondLim*_20200129_*';
-outputdir = fullfile(dimsynthpath, 'dimsynth', 'results', 'IFToMMDACH_Vgl_Winkel_20200129');
+filter = 'IFToMMDACH_Vgl_Winkel*_20200131_nachts_*';
+outputdir = fullfile(dimsynthpath, 'dimsynth', 'results', 'IFToMMDACH_Vgl_Winkel10_20200131_nachts');
 mkdirs(outputdir);
 %% Zusammenstellen der Ergebnisse
 reslist = dir(fullfile(importdir, filter));
@@ -43,11 +43,11 @@ fprintf('Ergebnistabellen nach %s geschrieben\n', RobTableFile);
 
 %% Bild für Materialausnutzung und Konditionszahl
 figure(1);clf;hold on;
-I_condlim = contains(ResTab_ges.OptName, 'KondLim1');
+% I_condlim = contains(ResTab_ges.OptName, 'KondLim1');
 for i = 1:length(Robots)
   RobName = Robots{i};
   I_Robi = strcmp(ResTab_ges.Name, RobName);
-  I = I_Robi&I_condlim;
+  I = I_Robi;%&I_condlim;
   plot(ResTab_ges.Kondition_phys(I), 100*ResTab_ges.Materialspannung(I), 'x');
 end
 % ylim([0,200]);
@@ -59,7 +59,7 @@ saveas(1,     fullfile(outputdir, sprintf('Materialbeanspruchung_vs_Jacobi')));
 export_fig(1, fullfile(outputdir, sprintf('Materialbeanspruchung_vs_Jacobi.png')));
 %% Bild für Anzahl erfolgreicher Parametersätze
 figure(2);clf;hold on;
-I_condlim = contains(ResTab_ges.OptName, 'KondLim1');
+% I_condlim = contains(ResTab_ges.OptName, 'KondLim1');
 [tokens,~] = regexp(ResTab_ges.OptName,'.*Winkel(\d+).*', 'tokens','match');
 Winkel_ges = NaN(length(ResTab_ges.OptName), 1);
 for i = 1:length(tokens)
@@ -69,7 +69,7 @@ quota = ResTab_ges.num_succ ./ (ResTab_ges.num_fail + ResTab_ges.num_succ);
 for i = 1:length(Robots)
   RobName = Robots{i};
   I_Robi = strcmp(ResTab_ges.Name, RobName);
-  I = I_Robi&I_condlim;
+  I = I_Robi;%&I_condlim;
   plot(Winkel_ges(I), 100*quota(I), 'x');
 end
 legend(Robots);
@@ -78,3 +78,11 @@ ylabel('Erfolgreiche Partikel (in Prozent)');
 grid on;
 saveas(2,     fullfile(outputdir, sprintf('Vergleich_Anteil_Erfolgreich')));
 export_fig(2, fullfile(outputdir, sprintf('Vergleich_Anteil_Erfolgreich.png')));
+
+%% Bild für Kinematikparameter
+
+for i = 1:length(Robots)
+  
+  
+  
+end
