1c34ce990f8ec27a75575cf01e6be507a0730bbf
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-02-05 15:48:46 +0100

    Sichtung der letzten Ergebnisse

--- berechnungen/results_repeat_evals.m
+++ berechnungen/results_repeat_evals.m
@@ -6,8 +6,8 @@ clc
 
 dimsynthpath = fileparts(which('structgeomsynth_path_init.m'));
 importdir = fullfile(dimsynthpath, 'dimsynth', 'results');
-filter = 'IFToMMDACH_Vgl_Winkel*_20200131_nachts_*';
-outputdir = fullfile(dimsynthpath, 'dimsynth', 'results', 'IFToMMDACH_Vgl_Winkel10_20200131_nachts');
+filter = 'IFToMMDACH_Vgl_Winkel*_20200203_nachts_*';
+outputdir = fullfile(dimsynthpath, 'dimsynth', 'results', 'IFToMMDACH_Vgl_20200203_nachts');
 mkdirs(outputdir);
 %% Zusammenstellen der Ergebnisse
 reslist = dir(fullfile(importdir, filter));
@@ -40,18 +40,24 @@ for i = 1:length(Robots)
   writetable(ResTab_Robi, RobTableFile, 'Delimiter', ';');
 end
 fprintf('Ergebnistabellen nach %s geschrieben\n', RobTableFile);
-
+% Merke, welche Optimierungen ein gültiges Endergebnis haben
+I_feasible = ResTab_ges.Fval_Opt < 1e3;
 %% Bild für Materialausnutzung und Konditionszahl
 figure(1);clf;hold on;
+
 % I_condlim = contains(ResTab_ges.OptName, 'KondLim1');
+hdl_feas = NaN(length(Robots),1);
 for i = 1:length(Robots)
   RobName = Robots{i};
   I_Robi = strcmp(ResTab_ges.Name, RobName);
   I = I_Robi;%&I_condlim;
-  plot(ResTab_ges.Kondition_phys(I), 100*ResTab_ges.Materialspannung(I), 'x');
+  set(gca, 'ColorOrderIndex', i);
+  hdl_feas(i) = plot(ResTab_ges.Kondition_phys(I&I_feasible), 100*ResTab_ges.Materialspannung(I&I_feasible), '^');
+  set(gca, 'ColorOrderIndex', i);
+  plot(ResTab_ges.Kondition_phys(I&~I_feasible), 100*ResTab_ges.Materialspannung(I&~I_feasible), 'x');
 end
 % ylim([0,200]);
-legend(Robots);
+legend(hdl_feas, Robots);
 xlabel('Konditionszahl Jacobi');
 ylabel('Materialbeanspruchung in Prozent');
 grid on;
@@ -62,28 +68,33 @@ export_fig(1, fullfile(outputdir, sprintf('Materialbeanspruchung_vs_Jacobi.png')
 figure(2);clf;hold on;
 
 % I_condlim = contains(ResTab_ges.OptName, 'KondLim1');
+hdl_feas = NaN(length(Robots),1);
 for i = 1:length(Robots)
   RobName = Robots{i};
   I_Robi = strcmp(ResTab_ges.Name, RobName);
   I = I_Robi;%&I_condlim;
-  plot(ResTab_ges.Kondition_phys(I), ResTab_ges.Energie_phys(I), 'x');
+  set(gca, 'ColorOrderIndex', i);
+  hdl_feas(i) = plot(ResTab_ges.Kondition_phys(I&I_feasible), ResTab_ges.Energie_phys(I&I_feasible), '^');
+  set(gca, 'ColorOrderIndex', i);
+  plot(ResTab_ges.Kondition_phys(I&~I_feasible), ResTab_ges.Energie_phys(I&~I_feasible), 'x');
 end
 % ylim([0,200]);
-legend(Robots);
+legend(hdl_feas, Robots);
 xlabel('Konditionszahl Jacobi');
 ylabel('Energie in J');
 grid on;
 saveas(2,     fullfile(outputdir, sprintf('Energie_vs_Jacobi')));
 export_fig(2, fullfile(outputdir, sprintf('Energie_vs_Jacobi.png')));
 
-%% Bild für Anzahl erfolgreicher Parametersätze
-figure(3);clf;hold on;
+%% Bild für Eigenschaften über Neigungswinkel
+figure(3);clf;
 % I_condlim = contains(ResTab_ges.OptName, 'KondLim1');
 [tokens,~] = regexp(ResTab_ges.OptName,'.*Winkel(\d+).*', 'tokens','match');
 Winkel_ges = NaN(length(ResTab_ges.OptName), 1);
 for i = 1:length(tokens)
   Winkel_ges(i) = str2double(tokens{i}{1});
 end
+subplot(2,2,1); hold on;
 quota = ResTab_ges.num_succ ./ (ResTab_ges.num_fail + ResTab_ges.num_succ);
 for i = 1:length(Robots)
   RobName = Robots{i};
@@ -95,6 +106,59 @@ legend(Robots);
 xlabel('Neigungswinkel');
 ylabel('Erfolgreiche Partikel (in Prozent)');
 grid on;
+title('Erfolgsquote vs Schwenkwinkel');
+
+subplot(2,2,2); hold on;
+hdl_feas = NaN(length(Robots),1);
+for i = 1:length(Robots)
+  RobName = Robots{i};
+  I_Robi = strcmp(ResTab_ges.Name, RobName);
+  I = I_Robi;%&I_condlim;
+  set(gca, 'ColorOrderIndex', i);
+  hdl_feas(i) = plot(Winkel_ges(I&I_feasible), ResTab_ges.Kondition_phys(I&I_feasible), '^');
+  set(gca, 'ColorOrderIndex', i);
+  plot(Winkel_ges(I&~I_feasible), ResTab_ges.Kondition_phys(I&~I_feasible), 'x');
+end
+legend(hdl_feas, Robots);
+xlabel('Neigungswinkel');
+ylabel('Konditionszahl Endergebnis');
+grid on;
+title('Konditionszahl vs Schwenkwinkel');
+
+subplot(2,2,3); hold on;
+hdl_feas = NaN(length(Robots),1);
+for i = 1:length(Robots)
+  RobName = Robots{i};
+  I_Robi = strcmp(ResTab_ges.Name, RobName);
+  I = I_Robi;%&I_condlim;
+  set(gca, 'ColorOrderIndex', i);
+  hdl_feas(i) = plot(Winkel_ges(I&I_feasible), ResTab_ges.Energie_phys(I&I_feasible), '^');
+  set(gca, 'ColorOrderIndex', i);
+  plot(Winkel_ges(I&~I_feasible), ResTab_ges.Energie_phys(I&~I_feasible), 'x');
+end
+legend(hdl_feas, Robots);
+xlabel('Neigungswinkel');
+ylabel('Energie Endergebnis in J');
+grid on;
+title('Energie vs Schwenkwinkel');
+
+subplot(2,2,4); hold on;
+hdl_feas = NaN(length(Robots),1);
+for i = 1:length(Robots)
+  RobName = Robots{i};
+  I_Robi = strcmp(ResTab_ges.Name, RobName);
+  I = I_Robi;%&I_condlim;
+  set(gca, 'ColorOrderIndex', i);
+  hdl_feas(i) = plot(Winkel_ges(I&I_feasible), 100*ResTab_ges.Materialspannung(I&I_feasible), '^');
+  set(gca, 'ColorOrderIndex', i);
+  plot(Winkel_ges(I&~I_feasible), 100*ResTab_ges.Materialspannung(I&~I_feasible), 'x');
+end
+legend(hdl_feas, Robots);
+xlabel('Neigungswinkel');
+ylabel('Materialbelastung Endergebnis in %');
+grid on;
+title('Materialbelastung vs Schwenkwinkel');
+
 saveas(3,     fullfile(outputdir, sprintf('Vergleich_Anteil_Erfolgreich')));
 export_fig(3, fullfile(outputdir, sprintf('Vergleich_Anteil_Erfolgreich.png')));
 
