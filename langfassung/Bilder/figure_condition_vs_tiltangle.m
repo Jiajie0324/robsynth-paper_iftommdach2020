dd8c28214b8f078726416cb3870f6cfcfee81cda
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-02-14 16:00:18 +0100

    Daten in Schwenkwinkel-Auswertung anders angeordnet, kleinere Korrekturen im Ergebnis-Kapitel

--- langfassung/Bilder/figure_condition_vs_tiltangle.m
+++ langfassung/Bilder/figure_condition_vs_tiltangle.m
@@ -18,7 +18,7 @@ colors = [[0 80 155 ]/255; ...%imesblau
 %% Zusammenfassungen der bisherige Versuche laden
 dimsynthpath = fileparts(which('structgeomsynth_path_init.m'));
 resdirtotal = fullfile(dimsynthpath, 'dimsynth', 'results');
-resdirs = {'IFToMMDACH_Vgl_20200206_nachts'};
+resdirs = {'IFToMMDACH_Vgl_20200206_nachts', 'IFToMMDACH_Vgl_20200212_nachts', 'IFToMMDACH_Vgl_20200211'};
 for i = 1:length(resdirs)
   tablepath = fullfile(resdirtotal, resdirs{i}, 'all_results.csv');
   ResTab_i = readtable(tablepath, 'HeaderLines', 1);
@@ -30,6 +30,7 @@ for i = 1:length(resdirs)
     ResTab_ges = [ResTab_ges; ResTab_i]; %#ok<AGROW>
   end
 end
+writetable(ResTab_ges, fullfile(outputdir, 'figure_condition_vs_tiltangle_data.csv'), 'Delimiter', ';');
 % Liste der Namen für die Legende bearbeiten
 Robots_TL_List = { ...
   {'P6PRRRRR6G8P3A1',   '6PRRRRR'}, ...
@@ -41,7 +42,20 @@ Robots_TL = Robots;
 for i = 1:length(Robots_TL_List)
   Robots_TL = strrep(Robots_TL, Robots_TL_List{i}{1}, Robots_TL_List{i}{2});
 end
-% Bild zeichnen
+%% Kurzauswertung machen
+TiltAngles = 180/pi*unique(ResTab_ges.MaxTiltAngle);
+I_valid = ResTab_ges.Fval_Opt < 1e3;
+sum_total = 0;
+for j = 1:length(Robots)
+  I_robj = strcmp(ResTab_ges.Name, Robots{j});
+  for i = 1:length(TiltAngles)
+    sum_total = sum_total + sum(I_TAi&I_robj);
+    I_TAi = abs(ResTab_ges.MaxTiltAngle*180/pi - TiltAngles(i)) < 1e-6;
+    fprintf('Rob %s, Winkel %1.0f: %d/%d Erfolgreich\n', Robots{j}, TiltAngles(i), sum(I_valid&I_robj&I_TAi), sum(I_robj&I_TAi));
+  end
+end
+fprintf('%d Einträge mit Filter gezählt. %d Einträge insgesamt\n', sum_total, size(ResTab_ges,1));
+%% Bild zeichnen
 figure(1);clf;hold on;
 plotsymbollist = {'^', 'o', 'x', 's'};
 shift = [-1.5, -0.5, .5, 1.5];
@@ -56,13 +70,28 @@ end
 grid on;
 h = legend(Robots_TL, 'orientation', 'horizontal'); 
 h.ItemTokenSize(1) = 15; %https://de.mathworks.com/matlabcentral/answers/438845-change-figure-legend-horizontal-spacing#answer_398095
-xlabel('Max. Tilt Angle in Traj. in deg');
+xlh = xlabel('Maximum Tilt Angle in Trajectory in deg');
 ylabel('cond(J)');
-xlim([35, 95]);
-ylim([0, 230]);
+xlim([-5, 89]);
+ylim([8, 230]);
+
+% Grid und Achseneinteilung so verändern, dass es Abschnittsweise für jeden
+% Winkelschritt eine Sammlung von Werten gibt
+TAintermediate = TiltAngles(1:end-1)+diff(TiltAngles)/2;
+set(gca, 'xtick', TAintermediate, 'xticklabel', {})
+[X_off, X_slope] = get_relative_position_in_axes(gca, 'x');
+[Y_off, Y_slope] = get_relative_position_in_axes(gca, 'y'); % Funktioniert nicht bei Log Scale
+set(xlh, 'position', [X_off+X_slope*(0), 5,1]);
+for i = 1:length(TiltAngles)-1
+  text(TiltAngles(i)-3, 6.5, sprintf('%1.0f',TiltAngles(i)), 'FontSize', 8);
+end
+
+set(gca, 'YScale', 'log'); % Zur Übersichtlichkeit -> Mehr Werte in kleinem Bereich
+set(gca, 'ytick', [10, 100])
+
 figure_format_publication(gca);
 set_size_plot_subplot(1,8,4.5,gca,...
   0.13,0.01,0.11,0.18,0,0); % bl,br,hu,hd,bdx,bdy
-set(h, 'Position', [0.15,0.93,0.7,0.05]);
+set(h, 'Position', [0.17,0.93,0.7,0.05]);
 
 export_fig(1, fullfile(outputdir, sprintf('figure_condition_vs_tiltangle.pdf')));
--- langfassung/Bilder/figure_energy_vs_tiltangle.m
+++ langfassung/Bilder/figure_energy_vs_tiltangle.m
@@ -5,6 +5,7 @@
 
 clear
 clc
+close all
 
 outputdir = fileparts(which('figure_energy_vs_tiltangle.m'));
 
@@ -17,7 +18,7 @@ colors = [[0 80 155 ]/255; ...%imesblau
 %% Zusammenfassungen der bisherige Versuche laden
 dimsynthpath = fileparts(which('structgeomsynth_path_init.m'));
 resdirtotal = fullfile(dimsynthpath, 'dimsynth', 'results');
-resdirs = {'IFToMMDACH_Vgl_20200206_nachts'};
+resdirs = {'IFToMMDACH_Vgl_20200206_nachts', 'IFToMMDACH_Vgl_20200212_nachts', 'IFToMMDACH_Vgl_20200211'};
 for i = 1:length(resdirs)
   tablepath = fullfile(resdirtotal, resdirs{i}, 'all_results.csv');
   ResTab_i = readtable(tablepath, 'HeaderLines', 1);
@@ -40,7 +41,7 @@ Robots_TL = Robots;
 for i = 1:length(Robots_TL_List)
   Robots_TL = strrep(Robots_TL, Robots_TL_List{i}{1}, Robots_TL_List{i}{2});
 end
-% Bild zeichnen
+%% Bild zeichnen
 figure(1);clf;hold on;
 plotsymbollist = {'^', 'o', 'x', 's'};
 shift = [-1.5, -0.5, .5, 1.5];
@@ -55,10 +56,22 @@ end
 grid on;
 h = legend(Robots_TL, 'orientation', 'horizontal'); 
 h.ItemTokenSize(1) = 15; %https://de.mathworks.com/matlabcentral/answers/438845-change-figure-legend-horizontal-spacing#answer_398095
-xlabel('Max. Tilt Angle in Traj. in deg');
+xlh = xlabel('Maximum Tilt Angle in Trajectory in deg');
 ylabel('Energy in J');
-xlim([30, 95]);
-ylim([70, 240]);
+xlim([-5, 89]);
+ylim([60, 240]);
+% Grid und Achseneinteilung so verändern, dass es Abschnittsweise für jeden
+% Winkelschritt eine Sammlung von Werten gibt
+TiltAngles = 180/pi*unique(ResTab_ges.MaxTiltAngle);
+TAintermediate = TiltAngles(1:end-1)+diff(TiltAngles)/2;
+set(gca, 'xtick', TAintermediate, 'xticklabel', {})
+[X_off, X_slope] = get_relative_position_in_axes(gca, 'x');
+[Y_off, Y_slope] = get_relative_position_in_axes(gca, 'y');
+set(xlh, 'position', [X_off+X_slope*(0), Y_off+Y_slope*(-1.25),1]);
+for i = 1:length(TiltAngles)-1
+  text(TiltAngles(i)-3, Y_off+Y_slope*(-1.1), sprintf('%1.0f',TiltAngles(i)), 'FontSize', 8);
+end
+
 figure_format_publication(gca);
 set_size_plot_subplot(1,8,4.5,gca,...
   0.13,0.01,0.11,0.18,0,0); % bl,br,hu,hd,bdx,bdy
