9c20f1bfa6091b2c9a0f7a38d74121546237b9d9
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-02-15 12:00:36 +0100

    Kommentare ergänzt, Liste der Schwenkwinkel vollständig (vorher einzelne getrennte Durchläufe)

--- config_dimsynth/cds_example_3T3R_PKM_wiederholbarkeit.m
+++ config_dimsynth/cds_example_3T3R_PKM_wiederholbarkeit.m
@@ -1,14 +1,13 @@
 % Einstellungen für komb. Struktur- und Maßsynthese für 3T3R PKM
 % Auswertung Bezüglich Schwenkwinkeln und Konditionszahl
+% Wiederholte Durchführung der Optimierung
 
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2020-01
 % (C) Institut für Mechatronische Systeme, Universität Hannover
 
 clc
 clear
-% serroblib_create_template_functions({'S6PRRRRR6', 'S6PRRRRR6V2', 'S6RRRRRR10', 'S6RRRRRR10V3'}, false, true);
-% mex_all_matlabfcn_in_dir('/home/schappler/IMES/REPO/robotersynthese/serrob_mdlbib/mdl_6dof/S6RRRRRR10/hd');
-% mex_all_matlabfcn_in_dir('/home/schappler/IMES/REPO/robotersynthese/serrob_mdlbib/mdl_6dof/S6PRRRRR6/hd');
+
 % Aufgaben-FG
 DoF = [1 1 1 1 1 1];
 Traj_no = 2;
@@ -16,7 +15,7 @@ Traj_no = 2;
 Set = cds_settings_defaults(struct('DoF', DoF));
 Set.task.Ts = 1e-2;
 Set.task.Tv = 1e-1;
-Set.task.profile = 1;
+Set.task.profile = 1; % Trapez
 
 Set.optimization.objective = 'energy';
 Set.optimization.NumIndividuals = 100;
@@ -29,7 +28,7 @@ Set.optimization.platform_size = true;
 Set.optimization.platform_size_limits = [0.3 1];
 Set.optimization.base_morphology = true;
 Set.optimization.platform_morphology = true;
-Set.optimization.movebase = false;
+Set.optimization.movebase = false; % PKM bleibt immer in der Mitte
 Set.optimization.constraint_obj(4) = 1e3; % Konditionszahl darf nicht total schlecht sein
 Set.optimization.constraint_link_yieldstrength = 1.0; % Beachte Materialbeanspruchung
 Set.general.max_retry_bestfitness_reconstruction = 1;
@@ -47,9 +46,9 @@ Set.structures.whitelist = { ...
 % Optimierung mehrfach wiederholen
 for repno = 1:10
   % Verschiedene Schwenkwinkel durchgehen
-  for maxangle = [60, 30, 45 75 90]
+  for maxangle = [0, 10, 20, 60, 30, 45, 75, 90]
     % Einstellungen anpassen
-    Set.optimization.optname = sprintf('IFToMMDACH_Vgl_Winkel%d_20200206_nachts_Wdh%d', ...
+    Set.optimization.optname = sprintf('IFToMMDACH_Vgl_Winkel%d_Wdh%d', ...
       maxangle, repno);
     Set.task.maxangle = maxangle*pi/180; 
     Traj = cds_gen_traj(DoF, Traj_no, Set.task);
